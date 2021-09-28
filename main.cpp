d10c24a7117be32b669597a1c30db275db1e41a8
ghendersonnc <ghendersonnc@gmail.com>
2021-09-27 23:11:45 -0400

    test2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main() {
     std::cout << "testing clion vcs" << std::endl;
 
-    Person person1("John Smith", 22);
+    Person person1("John Smiths", 22);
 
     std::cout << person1.getName() << std::endl;
     std::cout << person1.getAge() << std::endl;
